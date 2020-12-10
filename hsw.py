8e0701cb2214e8065a630dc0eadba89d683974a1
zrthstr <zrth1k@gmail.com>
2020-12-10 23:11:09 +0100

    add header compressing

--- hsw.py
+++ hsw.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import fileinput
+import sys
+import os
+import copy
+import pprint
 import uncurl
 import curlify
-import sys
-import copy
+import fileinput
 
 from requests import Request, Session, structures
 from loguru import logger as log
@@ -18,32 +20,29 @@ from loguru import logger as log
 
 
 # TODO:
-# start with http header diet
-#
-# next:
-# check if unculr treats cookie as regular header
 # clean error on non http(s)
+# add more error handeling
+# add more tests
 #
 # what else?
-# put data on diet?
-# remove referrer?
+# shorten needed headers?
+#  *useragent
+#  *cookies
 
 
-def send(seq):
-    # print(seq.headers)
+def send(seq, log_msg="headers"):
     resp = s.send(seq)
-    # log.info(
-    #    f"headers: {len(seq.headers)}, reply len: {len(resp.text)} status {resp.status_code}"
-    # )
+    log.trace(
+        f"{log_msg}: {len(seq.headers)}, reply len: {len(resp.text)} status {resp.status_code}"
+    )
     return resp.status_code, len(resp.text)
 
 
 def parse(line):
-    log.info(f"Processing line {line}")
+    #log.info(f"Processing line {line}")
     context = uncurl.parse_context(line)
     ## uncurl doesn't guarantee an url scheme while curlify insists on having one
     if not context.url.startswith("http"):
-        # context.url = "http://" + context.url ## context is RO...
         context = context._replace(url="http://" + context.url)
     req = Request(
         context.method.upper(), context.url, headers=context.headers, data=context.data
@@ -63,35 +62,49 @@ def is_same_without(prep, without):
     return True
 
 
-def triage(prep_full, full_status_code, full_len):
+#def triage(prep_full, full_status_code, full_len):
+def triage(prep_full, _ , full_len):
     header_count = len(prep_full.headers)
     if len(prep_full.headers) == 1:
         return prep_full
 
-    log.info(f"Starting triage with: {header_count} headers")
+    log.debug(f"Starting triage with: {header_count} headers")
     # for h in range(0, header_count):
 
     obsolete_headers = structures.CaseInsensitiveDict()
     needed_headers = structures.CaseInsensitiveDict()
     for k, v in prep_full.headers.items():
         if is_same_without(prep_full, k):
-            log.info(f"Found needed headder: {k, v}")
+            log.debug(f"Found needed headder: {k, v}")
             needed_headers[k] = v
         else:
-            log.info(f"Found obsolete headder: {k, v}")
+            log.debug(f"Found obsolete headder: {k, v}")
             obsolete_headers[k] = v
             # del header
 
-    import pprint
-
-    log.debug(f"Needed:")
-    pprint.pprint(dict(needed_headers), width=9999, indent=4)
-
-    log.debug(f"Obsolete:")
-    pprint.pprint(dict(obsolete_headers), width=-1, indent=4)
+    #log.debug(f"Needed: { pprint.pformat(dict(needed_headers), width=9999, indent=4)}")
+    #log.debug(f"Obsolete: {pprint.pformat(dict(obsolete_headers), width=-1, indent=4)}")
 
     prep_full.headers = needed_headers
     return prep_full
+
+def condense(prep_full, status_code, full_len):
+    max_user_agent_len = 15
+    org_user_agent = prep_full.headers["user-agent"]
+    log.debug(f'condensing User-Agent: {org_user_agent}')
+    if " " in org_user_agent.lower():
+        prep_full.headers["user-agent"] = org_user_agent.split()[0]
+    elif len(org_user_agent) > max_user_agent_len:
+        prep_full.headers["user-agent"] = org_user_agent[0:max_user_agent_len]
+    else:
+        # no change, no test needed
+        return prep_full
+
+    # make sure condensing has no effect
+    new_status, new_len = send(prep_full, log_msg="condenseing")
+    if not (new_status, new_len) == (status_code, full_len):
+        prep_full.headers["user-agent"] = org_user_agent
+    return  prep_full
 
 
 def process(line):
@@ -104,29 +117,40 @@ def process(line):
     prep_plain = copy.deepcopy(prep_full)
     prep_plain.headers = structures.CaseInsensitiveDict()
 
-    log.info("Probing full request")
+    log.debug("Probing full request")
     full_status_code, full_len = send(prep_full)
-    log.info("Probing plain request")
+    log.debug("Probing plain request")
     plain_status_code, plain_len = send(prep_plain)
 
     if not full_status_code == 200:
-        log.error("Aborting: Original query not of status 200: " + full_status_code)
-        #### must thest if this end the program
-        #### if not do so
-        #### sys.exit()
+        log.error(f"Aborting: supplied curl command not exiting with status 200: {full_status_code}")
+        sys.exit()
 
-    elif plain_status_code == full_status_code and full_len == plain_len:
-        log.info("Done. Headers have no effect", dir(prep_plain))
+    if plain_status_code == full_status_code and full_len == plain_len:
+        log.debug("Done. All headers have no effect")
         return prep_plain
 
-    else:
-        log.info("Some Headders needed. Starting search")
-        prep_minimum = triage(prep_full, full_status_code, full_len)
-        return prep_minimum
+    log.debug("Some Headders appear relevant. Starting triage")
+    prep_minimum = triage(prep_full, full_status_code, full_len)
+
+
+    if 'user-agent' in prep_minimum.headers:
+        prep_minimum = condense(prep_minimum, full_status_code, full_len)
+
+    return prep_minimum
 
 
 if __name__ == "__main__":
+
+    # for fc + vim usecase:
+    # detect if calling process is editor
+    # set non verbose logging then
+    if os.readlink("/proc/%s/exe" % os.getppid()).endswith(("/vi", "/vim")):
+        for log_level in ["DEBUG", "INFO", "WARN"]:
+            log.disable(log_level)
+
     for line in fileinput.input():
+        log.debug(f"Processing line: {line}")
         request_obj = process(line)
         print(curlify.to_curl(request_obj))
         # try:
