8b3ed8fc82bf58eeb90eb3db622e19a74af34f28
zrthstr <zrth1k@gmail.com>
2020-12-10 23:31:01 +0100

    clean up

--- hsw.py
+++ hsw.py
@@ -3,10 +3,10 @@
 import sys
 import os
 import copy
-import pprint
+#import pprint
+import fileinput
 import uncurl
 import curlify
-import fileinput
 
 from requests import Request, Session, structures
 from loguru import logger as log
@@ -16,22 +16,16 @@ from loguru import logger as log
 # get rid of all curl paramters that dont seem to make a difference
 # write curl command back to stdout
 #
-# limitation: only a small subset of curl is supported, the parameters that come from chrome > copy_as_curl...
+# limitation: only a small subset of curl is supported,
+# the parameters that come from chrome > copy_as_curl...
 
 
-# TODO:
-# clean error on non http(s)
-# add more error handeling
-# add more tests
-#
-# what else?
-# shorten needed headers?
-#  *useragent
-#  *cookies
+# what's next?
+#  condense cookies?
 
 
 def send(seq, log_msg="headers"):
-    resp = s.send(seq)
+    resp = session.send(seq)
     log.trace(
         f"{log_msg}: {len(seq.headers)}, reply len: {len(resp.text)} status {resp.status_code}"
     )
@@ -50,20 +44,20 @@ def parse(line):
     return req
 
 
-def is_same_without(prep, without):
+def is_same_without(prep, without, full_status_code, full_len):
     # print(dir(prep.headers))
     one_less = copy.deepcopy(prep)
     del one_less.headers[without]
 
     new_status, new_len = send(one_less)
     # print(prep.headers)
-    if new_status == new_status and new_len == full_len:
+    if new_status == full_status_code and new_len == full_len:
         return False
     return True
 
 
-#def triage(prep_full, full_status_code, full_len):
-def triage(prep_full, _ , full_len):
+def triage(prep_full, full_status_code, full_len):
+#def triage(prep_full, _ , full_len):
     header_count = len(prep_full.headers)
     if len(prep_full.headers) == 1:
         return prep_full
@@ -73,13 +67,13 @@ def triage(prep_full, _ , full_len):
 
     obsolete_headers = structures.CaseInsensitiveDict()
     needed_headers = structures.CaseInsensitiveDict()
-    for k, v in prep_full.headers.items():
-        if is_same_without(prep_full, k):
-            log.debug(f"Found needed headder: {k, v}")
-            needed_headers[k] = v
+    for headder_name, headder_value in prep_full.headers.items():
+        if is_same_without(prep_full, headder_name, full_status_code, full_len):
+            log.debug(f"Found needed headder: {headder_name, headder_value}")
+            needed_headers[headder_name] = headder_value
         else:
-            log.debug(f"Found obsolete headder: {k, v}")
-            obsolete_headers[k] = v
+            log.debug(f"Found obsolete headder: {headder_name, headder_value}")
+            obsolete_headers[headder_name] = headder_value
             # del header
 
     #log.debug(f"Needed: { pprint.pformat(dict(needed_headers), width=9999, indent=4)}")
@@ -88,7 +82,7 @@ def triage(prep_full, _ , full_len):
     prep_full.headers = needed_headers
     return prep_full
 
-def condense(prep_full, status_code, full_len):
+def condense(prep_full, full_status_code, full_len):
     max_user_agent_len = 15
     org_user_agent = prep_full.headers["user-agent"]
     log.debug(f'condensing User-Agent: {org_user_agent}')
@@ -102,17 +96,19 @@ def condense(prep_full, status_code, full_len):
 
     # make sure condensing has no effect
     new_status, new_len = send(prep_full, log_msg="condenseing")
-    if not (new_status, new_len) == (status_code, full_len):
+    if (new_status, new_len) != (full_status_code, full_len):
         prep_full.headers["user-agent"] = org_user_agent
     return  prep_full
 
 
 def process(line):
     req = parse(line)
-    global s
-    global full_status
-    global full_len
-    s = Session()
+    global session
+
+    #global full_status
+    #global full_len
+
+    session = Session()
     prep_full = req.prepare()
     prep_plain = copy.deepcopy(prep_full)
     prep_plain.headers = structures.CaseInsensitiveDict()
@@ -123,7 +119,7 @@ def process(line):
     plain_status_code, plain_len = send(prep_plain)
 
     if not full_status_code == 200:
-        log.error(f"Aborting: supplied curl command not exiting with status 200: {full_status_code}")
+        log.error(f"Error: supplied curl command not exiting with status 200: {full_status_code}")
         sys.exit()
 
     if plain_status_code == full_status_code and full_len == plain_len:
@@ -145,14 +141,16 @@ if __name__ == "__main__":
     # for fc + vim usecase:
     # detect if calling process is editor
     # set non verbose logging then
+
     if os.readlink("/proc/%s/exe" % os.getppid()).endswith(("/vi", "/vim")):
         for log_level in ["DEBUG", "INFO", "WARN"]:
             log.disable(log_level)
 
-    for line in fileinput.input():
-        log.debug(f"Processing line: {line}")
-        request_obj = process(line)
+    for curl_line in fileinput.input():
+        log.debug(f"Processing curl: {curl_line}")
+        request_obj = process(curl_line)
         print(curlify.to_curl(request_obj))
+
         # try:
         #    process(line)
         # except:
