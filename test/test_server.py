74efdd2f49c60436b6812e792bd5db4aed89136b
zrthstr <zrth1k@gmail.com>
2020-12-16 21:14:52 +0100

    new tests

--- test/test_server.py
+++ test/test_server.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python3
 
-# testing cases
-#
-# * return same for any request
-# * return other for any other but original request
-# * one headder needed
-# * all but one headder needed
-#
-# * insensitive user agent
-# * sesitive user agent
-# *  .. ?
+from random import random
 
-#from http.server import HTTPServer, BaseHTTPRequestHandler,  SimpleHTTPRequestHandler
 
-import sys
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from flask import Flask, request
+app = Flask(__name__, static_folder=None)
 
-def run(arg, server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-#def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    port = 8000 + arg
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    print(dir(httpd))
-    #httpd.handle_request()
 
-    #httpd.process_request()
-    httpd.serve_forever()
+#@app.route('/', methods=['GET'])
 
-def usage():
-    print("error:  TBD")
-    print(f"example: {sys.argv[0]} 0 1 2 [testcase]")
-    print(f"example: {sys.argv[0]} 3 4")
-    sys.exit()
+@app.route('/')
+def info_rsq():
+    #print(app.url_map)
+    return f"{str(app.url_map)}\n\n{str(request.headers)}\n"
 
-if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        usage()
-    for arg in sys.argv[1:]:
-        try:
-            arg = int(arg)
-            assert -1 < arg < 5
-            #assert -1 < arg < len(testcases)
-        except:
-            print(f"skipping invalid testcase: {arg}")
-            continue
-        run(arg)
+@app.route('/vanilla')
+def vanilla_rsq():
+    return "Vanilla\n"
+
+@app.route('/random')
+def random_rsq():
+    return str(random.randint(1E4,int(1E16))) + "\n"
+
+### some headers needed
+@app.route('/some')
+def some_headers_need_rsq():
+    if 'Needed-0' in request.headers and 'Needed-1' in request.headers:
+        if request.headers['Needed-0'] == 'Is-0' and request.headers['Needed-1'] == 'Is-1':
+            return "OKEY"
+    return "NOPE"
+
+## long user agent. only first N chars matters
+@app.route('/useragent')
+def user_agent_req():
+    if 'User-Agent' in request.headers:
+        if request.headers['User-Agent'].startswith("Mozilla"):
+            return "OKEY"
+    return "NOPE"
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
